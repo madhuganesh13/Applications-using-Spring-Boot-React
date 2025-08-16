a699acd3db39229c5db3d7338bc1f5435843bfc3
Madhu Ganesun R <160342606+madhuganesh13@users.noreply.github.com>
2025-08-17 03:00:47 +0530

    Update Api.js

--- EventPlanner application(spring+react)/EventPlanner-Frontend/src/Api.js
+++ EventPlanner application(spring+react)/EventPlanner-Frontend/src/Api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Use your Railway backend URL here
-const API_BASE = "https://eventplanner-backend-production-cd79.up.railway.app/api";
+const API_BASE = "https://applications-using-spring-boot-react-production.up.railway.app/api";
 
 const API = axios.create({
-  baseURL: "https://eventplanner-backend-production-cd79.up.railway.app/api",
+  baseURL: "https://applications-using-spring-boot-react-production.up.railway.app/api",
 });
 // Event APIs
 export const getEvents = (params) => API.get("/events", { params });
@@ -14,6 +14,7 @@ export const updateEvent = (id, data) => API.put(`/events/${id}`, data);
 export const deleteEvent = (id) => API.delete(`/events/${id}`);
 
 export default API;
+
 
 
 
