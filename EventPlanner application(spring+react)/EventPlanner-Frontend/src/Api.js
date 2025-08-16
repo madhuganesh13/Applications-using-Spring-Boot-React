63579e1d7ae28063627ca973a0ab7c325294bbdc
Madhu Ganesun R <160342606+madhuganesh13@users.noreply.github.com>
2025-08-16 19:13:09 +0530

    Update Api.js

--- EventPlanner application(spring+react)/EventPlanner-Frontend/src/Api.js
+++ EventPlanner application(spring+react)/EventPlanner-Frontend/src/Api.js
@@ -4,16 +4,17 @@ import axios from "axios";
 //const API_BASE = "https://eventplanner-backend-production-9fb3.up.railway.app/api/events";
 
 const API = axios.create({
-  baseURL: "https://eventplanner-backend-production-cd79.up.railway.app/api",
+  baseURL: "https://eventplanner-backend-production-cd79.up.railway.app/api/events",
 });
 // Event APIs
-export const getEvents = (params) => API.get("/events", { params });
+//export const getEvents = (params) => API.get("/events", { params });
 export const getEvent = (id) => API.get(`/events/${id}`);
 export const createEvent = (data) => API.post("/events", data);
 export const updateEvent = (id, data) => API.put(`/events/${id}`, data);
 export const deleteEvent = (id) => API.delete(`/events/${id}`);
 
 export default API;
+
 
 
 
