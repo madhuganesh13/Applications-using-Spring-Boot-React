ed85d1c5d114c7774dcec486795c2c46be96e21e
Madhu Ganesun R <160342606+madhuganesh13@users.noreply.github.com>
2025-08-18 13:37:21 +0530

    Update client.ts

--- student-management/student-management-Frontend/src/api/client.ts
+++ student-management/student-management-Frontend/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
-  headers: { 'Content-Type': 'application/json' }
+baseURL: import.meta.env.VITE_API_BASE || '/api',
+headers: { 'Content-Type': 'application/json' }
 })
 
 export default client
