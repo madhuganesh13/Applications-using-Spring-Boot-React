b58c4818cd4c96d2a4d8e4e6a0ce56042a355e4a
Madhu Ganesun R <160342606+madhuganesh13@users.noreply.github.com>
2025-09-09 11:33:44 +0530

    Update data.sql

--- student-management/student-management-Backend/src/main/resources/data.sql
+++ student-management/student-management-Backend/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO students (id, first_name, last_name, email, date_of_birth, gender, department, gpa) VALUES
-  (1, 'Aisha', 'Khan', 'aisha.khan@example.com', '2002-01-15', 'Female', 'Computer Science', 9.1),
-  (2, 'Rohan', 'Verma', 'rohan.verma@example.com', '2001-05-20', 'Male', 'Electronics', 8.3),
-  (3, 'Leena', 'Patel', 'leena.patel@example.com', '2003-09-30', 'Female', 'Mechanical', 7.6);
+  (1, 'Madhu', 'Ganesh', 'madhuganesunedu@gmail.com', '2005-04-19', 'male', 'Computer Science', 8.39),
+  (2, 'Jithu', 'Varadan', 'jithuvaradan@gmail.com', '2005-02-26', 'Male', 'Electronics', 7.5),
+  (3, 'Rubax', 'Rajesh', 'rubax2002@gmail.com', '2003-09-30', 'male', 'computer science', 6.6);
+
